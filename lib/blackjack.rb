1aaf241a66dcfeabd233157e10a2409fd587b522
Austin Kohler <austinckohler@gmail.com>
2020-03-02 02:27:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,15 +29,15 @@ def initial_round
 end
 
 def hit?(total)
- prompt_user
-  answer = get_user_input
-  if answer == "s"
-    total
-  elsif answer == "h"
-    deal_card + total
-  else 
-    invalid_command
-  end
+  prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+     total += deal_card
+   elsif user_input == "s"
+     total
+   else
+     invalid_command
+   end
 end
 
 def invalid_command
